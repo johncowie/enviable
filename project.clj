ed3dc5fea9f1d7c23384ac5b70cf453b3f21b20c
John Cowie <john@iamfy.co>
2021-02-17 09:44:02 +0000

    Add group to project name

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject enviable "0.1.0-SNAPSHOT"
+(defproject johncowie/enviable "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.stuartsierra/component "1.0.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :repl-options {:init-ns enviable.core}
   :main nil
   :source-paths ["src"]
   :profiles {:dev {:source-paths ["src" "examples" "test"]
+                   :dependencies [[com.stuartsierra/component "1.0.0"]]
                    :aliases      {"example.basic"     ["run" "-m" "enviable.examples.basic"]
                                   "example.component" ["run" "-m" "enviable.examples.component"]}}}
   )
