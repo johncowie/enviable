e73f396b89bac0d9d9f9a6595c157016d2e7af5e
John <john.a.cowie@gmail.com>
2021-02-21 17:17:26 +0000

    Set real deployment token

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject johncowie/enviable "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A user-friendly and composable way of configuring clojure apps"
+  :url "https://github.com/johncowie/enviable"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
